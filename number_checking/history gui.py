0a89824342e1c103bd05d250a5b76022f3079e54
hibbertm71030 <60630392+hibbertm71030@users.noreply.github.com>
2021-03-15 10:51:20 +1300

    working histroy

--- number_checking/history gui.py
+++ number_checking/history gui.py
@@ -12,9 +12,7 @@ class Converter:
         self.all_calculations = ['1 degrees F is -17.2 degrees C',
                                  '2 degrees F is -16.7 degrees C',
                                  '8 degrees F is -13.3 degrees C',
-                                 '-10 degrees F is -23.3 degrees C',
-                                 '-74 degrees F is -58.9 degrees C',
-                                 '56 degrees F is 13.3 degrees C']
+                                ]
 
         # converter GUI
         self.converter_frame = Frame(width=500, height=500, bg=background_colour,
@@ -29,17 +27,16 @@ class Converter:
         self.temp_converter_label.grid(row=0)
 
         # history button
-        self.history_button = Button(self.converter_frame, text="history",
-                                  padx=10, pady=10, command=self.history)
+        self.history_button = Button(self.converter_frame, text="history", padx=10, pady=10,
+                                     command=lambda: self.history(self.all_calculations))
         self.history_button.grid(row=1)
 
-    def history(self):
-        print("you asked for History")
-        get_history = history(self)
-        get_history.history_text.configure(text="History text goes here")
+    def history(self, calc_history):
+        History(self, calc_history)
 
-class history:
-    def __init__(self, partner):
+
+class History:
+    def __init__(self, partner, calc_history):
 
         background = "light blue"
 
@@ -61,9 +58,29 @@ class history:
         self.history_label.grid(row=0)
 
         # history text
-        self.history_text = Label(self.history_frame, text="",
-                               justify=LEFT, width=40, bg=background, wrap=250)
+        self.history_text = Label(self.history_frame, text="Here are your most recent calculations,"
+                                                           " to save all your calculations press export.",
+                                 font="arial 10", justify=LEFT, width=40, bg=background, wrap=250)
         self.history_text.grid(column=0, row=1)
+
+        # generate string from calc list
+        history_string = ""
+
+        if len(calc_history) >= 7:
+            for item in range(0, 7):
+                history_string += calc_history[len(calc_history) - item - 1]+"\n"
+
+        else:
+            for item in calc_history:
+                history_string += calc_history[len(calc_history) - calc_history.index(item) - 1] + "\n"
+                self.history_text.config(text="Here is your calculation history, "
+                                              "you can save this data by pressing export.")
+
+
+        # calc history label
+        self.calc_label = Label(self.history_frame, text=history_string,
+                                bg=background, font="Arial 10", justify=LEFT)
+        self.calc_label.grid(row=2)
 
         # export/dismiss buttons frame (row 3)
         self.export_dismiss_frame = Frame(self.history_frame)
